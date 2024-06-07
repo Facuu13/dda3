a257589dbae81dfd76fa01a6a1bd2e0e6f79b15b
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-06-07 14:08:23 -0300

    mostramos la informacion de los sensores

--- frontend/sensor-dashboard/src/components/NodeInfo.js
+++ frontend/sensor-dashboard/src/components/NodeInfo.js
@@ -2,29 +2,69 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const NodeInfo = () => {
-  const [nodeInfo, setNodeInfo] = useState(null);
+    const [dispositivos, setDispositivos] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/mediciones')
+    axios.get('/api/dispositivos')
       .then(response => {
-        const latestEntry = response.data[response.data.length - 1];
-        setNodeInfo(latestEntry);
+        const dispositivos = response.data;
+        setDispositivos(dispositivos);
       })
       .catch(error => {
-        console.error('Error fetching node info:', error);
+        console.error('Error fetching devices:', error);
       });
   }, []);
 
-  if (!nodeInfo) {
+  const [mediciones, setMediciones] = useState({});
+
+  useEffect(() => {
+    dispositivos.forEach(dispositivo => {
+      axios.get(`/api/mediciones/${dispositivo}`)
+        .then(response => {
+          const medidasDispositivo = response.data;
+          if (medidasDispositivo.length > 0) {
+            const ultimaMedida = medidasDispositivo[medidasDispositivo.length - 1];
+            setMediciones(prevState => ({
+              ...prevState,
+              [dispositivo]: ultimaMedida
+            }));
+          } else {
+            setMediciones(prevState => ({
+              ...prevState,
+              [dispositivo]: null
+            }));
+          }
+        })
+        .catch(error => {
+          console.error(`Error fetching measurements for device ${dispositivo}:`, error);
+        });
+    });
+  }, [dispositivos]);
+
+  if (dispositivos.length === 0) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h2>ID del Nodo: {nodeInfo.sensor_id}</h2>
-      <p>Última Temperatura: {nodeInfo.temperature}°C</p>
-      <p>Última Humedad: {nodeInfo.humidity}%</p>
-      <p>Última Medición: {new Date(nodeInfo.timestamp).toLocaleString()}</p>
+      <h2>Información de Nodos</h2>
+      {dispositivos.map((dispositivo, index) => (
+        <div key={index}>
+          <h3>ID del Nodo: {dispositivo}</h3>
+          {mediciones[dispositivo] ? (
+            <div>
+              <p>Última Temperatura: {mediciones[dispositivo].temperature}°C</p>
+              <p>Última Humedad: {mediciones[dispositivo].humidity}%</p>
+              <p>Última Medición: {new Date(mediciones[dispositivo].timestamp).toLocaleString()}</p>
+            </div>
+          ) : (
+            <div>
+              <p>No hay mediciones para este dispositivo</p>
+            </div>
+          )}
+          <hr />
+        </div>
+      ))}
     </div>
   );
 };
