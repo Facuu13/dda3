620e6119733ae4871a92ea0bcbe0ce70c8fec9a5
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-06-07 10:36:32 -0300

    se modularizo la parte mqtt

---
+++ backend/mqttHandler.js
@@ -0,0 +1,71 @@
+const mqtt = require('mqtt');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'facuu13',
+    password: 'talleres13',
+    database: 'sensor_data'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error al conectar a MySQL:', err);
+    } else {
+        console.log('Conectado a MySQL');
+    }
+});
+
+const brokerUrl = 'mqtt://localhost:1883';
+const options = {
+    username: 'facuu',
+    password: 'talleres13'
+};
+const client = mqtt.connect(brokerUrl, options);
+
+const sensorData = {};
+
+client.on('connect', () => {
+    console.log('Conectado al broker MQTT');
+    const topics = ['casaFacu/temp', 'casaFacu/hum'];
+    client.subscribe(topics, (err) => {
+        if (err) {
+            console.error('Error al suscribirse a los tópicos', err);
+        } else {
+            console.log(`Suscrito a los tópicos: ${topics.join(', ')}`);
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    try {
+        const data = JSON.parse(message.toString());
+        const { id, value } = data;
+
+        if (!sensorData[id]) {
+            sensorData[id] = {};
+        }
+
+        if (topic === 'casaFacu/temp') {
+            sensorData[id].temperature = value;
+        } else if (topic === 'casaFacu/hum') {
+            sensorData[id].humidity = value;
+        }
+
+        if (sensorData[id].temperature !== undefined && sensorData[id].humidity !== undefined) {
+            const query = 'INSERT INTO mediciones (sensor_id, temperature, humidity) VALUES (?, ?, ?)';
+            db.query(query, [id, sensorData[id].temperature, sensorData[id].humidity], (err, result) => {
+                if (err) {
+                    console.error('Error al insertar datos:', err);
+                } else {
+                    console.log(`Datos guardados: ID: ${id}, Temperatura: ${sensorData[id].temperature}, Humedad: ${sensorData[id].humidity}`);
+                    delete sensorData[id];
+                }
+            });
+        }
+    } catch (error) {
+        console.error('Error al procesar el mensaje:', error);
+    }
+});
+
+module.exports = client;
--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
 const mysql = require('mysql2');
+const router = express.Router();
 
 // Configuración de la conexión a MySQL
 const db = mysql.createConnection({
